416ef2b142b77e78875dcc484db20d935b10934e
p-sin <philip.sinfield@justice.gov.uk>
2025-07-07 17:49:50 +0100

    Set nox to use installed python version

--- noxfile.py
+++ noxfile.py
@@ -11,21 +11,21 @@ PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13"]
 PACKAGE_LOCATION = "."
 
 
-@nox.session(python=PYTHON_VERSIONS, tags=["lint"])
+@nox.session(tags=["lint"])
 def ruff(session: nox.Session) -> None:
     """Lint with ruff."""
     _install(session)
     _run(session, "ruff", "check", PACKAGE_LOCATION)
 
 
-@nox.session(python=PYTHON_VERSIONS, tags=["format"])
+@nox.session(tags=["format"])
 def isort(session: nox.Session) -> None:
     """Sort imports with isort."""
     _install(session)
     _run(session, "isort", PACKAGE_LOCATION)
 
 
-@nox.session(python=PYTHON_VERSIONS, tags=["test"])
+@nox.session(tags=["test"])
 def pytest(session: nox.Session) -> None:
     """Run the test suite with pytest."""
     args = session.posargs or ("--cov", "-m", "not e2e")
@@ -33,14 +33,14 @@ def pytest(session: nox.Session) -> None:
     _run(session, "pytest", *args)
 
 
-@nox.session(python=PYTHON_VERSIONS, tags=["typecheck"])
+@nox.session(tags=["typecheck"])
 def mypy(session: nox.Session) -> None:
     """Verify types using mypy (so it is static)."""
     _install(session)
     _run(session, "mypy", PACKAGE_LOCATION)
 
 
-@nox.session(python=PYTHON_VERSIONS, tags=["lint"])
+@nox.session(tags=["lint"])
 def yamllint(session: nox.Session) -> None:
     _install(session, "yamllint")
     _run(session, "yamllint", "src/", ".pre-commit-config.yaml")
